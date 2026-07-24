7102960a9e9e7af59265deec7eb58d3948c9ffba
elifdrms19 <elifdrms19@gmail.com>
2026-07-24 18:39:49 +0300

    Update settings.py

--- Kitap_kulubu/core/settings.py
+++ Kitap_kulubu/core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-key-for-local-development-only'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*]
 
 
 # Application definition
